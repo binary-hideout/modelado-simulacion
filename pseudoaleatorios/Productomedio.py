906ae522069ebd05fe6122de38c7a19089707233
DanielFl0 <MachoAlph@outlook.com>
2020-02-12 18:24:10 -0600

    Se agrego el archivo mcml.py y modificacion a productomedio.py

--- pseudoaleatorios/Productomedio.py
+++ pseudoaleatorios/Productomedio.py
@@ -1,11 +1,11 @@
 def generador(semilla1, semilla2):
-    resultado = semilla1 * semilla2
-    resultado = str(resultado)
-    if (len(resultado) % 2 == 0):
-        valor = producto_medio(resultado)
+    resultado_semillas = semilla1 * semilla2
+    resultado_semillas = str(resultado_semillas)
+    if (len(resultado_semillas) % 2 == 0):
+        valor = producto_medio(resultado_semillas)
     else:
-        valor = '0' + resultado
-        valor = producto_medio(resultado)
+        valor = '0' + resultado_semillas
+        valor = producto_medio(resultado_semillas)
     return valor
 
 def producto_medio(numero):
@@ -15,4 +15,14 @@ def producto_medio(numero):
     resultado = resultado / 100
     return resultado
 
-print(generador(12, 14))
+def numeros_pseudoaleatorios():
+    cantidad = int(input("Ingresa la cantidad de numeros pseudoaleatorios que desea generar: "))
+    semilla1 = int(input("Ingresa la primera semilla: "))
+    semilla2 = int(input("Ingresa la segunda semilla: "))
+    for i in range(cantidad):
+        resultado = generador(semilla1, semilla2)
+        semilla1 = semilla2
+        semilla2 = int(resultado * 100)
+        print(resultado)
+
+numeros_pseudoaleatorios()
