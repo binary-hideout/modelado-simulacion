f43c5479e02a3b32c038e7ca1604ab14a2bfde46
Daniel Contreras <danielcontreras@Vastagos-MacBook-Pro.local>
2020-02-25 22:23:53 -0600

    Prueba de stadístico implementado

--- pruebas_estadisticas/abajo_arriba.py
+++ pruebas_estadisticas/abajo_arriba.py
@@ -1,4 +1,18 @@
 import pandas as pd
+import math
+
+def main():
+    zn = float(input('Ingresa el valor de la chi cuadrada: '))
+    binario = lectura()
+    z = abajo_arriba(binario)
+    print(z)
+
+    if z < zn:
+        print('No se rechaza que son independientes')
+    else:
+        print('No pasa la prueba de las corridas')
+
+
 
 def lectura():
     leer = pd.read_csv(filepath_or_buffer = "Pruebas.csv", header = None)
@@ -11,6 +25,19 @@ def lectura():
         else:
             text = text + "1"
     
-    print(text)
+    return text
 
-lectura()
+def abajo_arriba(binario):
+    corridas = 1
+    x = binario[0]
+
+    for i in range(len(binario)):
+        if binario[i] == x:
+            corridas += 1
+            x = binario[i]
+
+    media = (2 * (len(binario) - 1)) / 3
+    varianza = (16 * media - 19) / 90
+    z = abs((corridas - media) / math.sqrt(varianza))
+    
+    return z
