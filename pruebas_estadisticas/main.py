05b8104f1ffa882073b9c38be86771f5c68c8885
netotz <neto.otz@hotmail.com>
2020-02-25 23:13:44 -0600

    Añadir menú para escoger cuál prueba usar
    Uso de la librería de CLI questionary

--- pruebas_estadisticas/main.py
+++ pruebas_estadisticas/main.py
@@ -1,3 +1,14 @@
 '''
 Módulo para mandar a llamar las funciones de la tarea de Pruebas estadísticas.
 '''
+
+from questionary import text, select, Choice
+
+menu = select(
+    'Seleccione la prueba estadística a usar:',
+    [
+        Choice('Abajo y arriba', 1),
+        Choice('Frecuencias', 2),
+        Choice('Salir', 0)
+    ]
+).ask()
