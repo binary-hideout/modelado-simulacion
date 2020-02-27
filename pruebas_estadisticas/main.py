9e661addf2dac0c48cdf3a8e2fb531262a59254f
netotz <neto.otz@hotmail.com>
2020-02-26 18:40:37 -0600

    Agregar opción cuando se escoje abajo_arriba

--- pruebas_estadisticas/main.py
+++ pruebas_estadisticas/main.py
@@ -4,7 +4,9 @@ Módulo para mandar a llamar las funciones de la tarea de Pruebas estadísticas.
 
 from questionary import text, select, Choice
 
+from validaciones import is_number
 import abajo_arriba
+import frecuencias
 
 menu = select(
     'Seleccione la prueba estadística a usar:',
@@ -12,12 +14,19 @@ menu = select(
         Choice('Abajo y arriba', 1),
         Choice('Frecuencias', 2),
         Choice('Salir', 0)
-    ]
+    ],
+    qmark='*'
 ).ask()
 
 # abajo y arriba
 if menu == 1:
-    abajo_arriba.main()
+    chi = text(
+        'Ingrese valor para chi cuadrada:',
+        validate=lambda n: True if is_number(n) else 'Por favor ingrese un número válido.',
+        qmark='>'
+    ).ask()
+    abajo_arriba.inicio(float(chi))
 # frecuencias
 elif menu == 2:
-    pass
+    #! error
+    frecuencias.main()
