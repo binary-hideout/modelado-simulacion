d19f05f75bf0fbd1a9f297b33ec6613d02b49c72
simsmilton <miltonrdz25@hotmail.com>
2020-02-26 22:27:14 -0600

    enter

--- pruebas_estadisticas/frecuencias.py
+++ pruebas_estadisticas/frecuencias.py
@@ -7,6 +7,7 @@ def main():
     cont3= 0 
     cont4= 0 
     cont5= 0
+    tot = 0
     path = os.path.dirname(__file__)
     file_path = os.path.join(path, 'Pruebas.csv')
     with open(file_path, encoding='utf-8-sig') as pruebas:
@@ -17,20 +18,23 @@ def main():
             datos.extend(numbers)
     for i in range(len(datos) - 1):
         if datos[i] >=0 and datos[i] <=0.2:
-           cont1=+1
+           cont1+=1
         elif datos [i] >0.2 and datos [i] <=0.4:
-            cont2=+1
+            cont2+=1
         elif datos[i] >0.4 and datos [i] <=0.6:
-            cont3=+1
+            cont3+=1
         elif datos[i]>0.6 and datos[i] <=0.8:
-            cont4=+1
+            cont4+=1
         elif datos[i]>0.8 and datos[i] <=1:
-            cont5=+1
-    frecEsp = 20;
+            cont5+=1
+    frecEsp = 20
     chi = float(input('Ingresa el valor de la chi cuadrada: '))
-    tot = (1/frecEsp)((cont1-frecEsp)^2+(cont2-frecEsp)^2+(cont3-frecEsp)^2+(cont4-frecEsp)^2+(cont5-frecEsp)^2)
-    print("Valor de X es"+tot)
+    print('Frecuencia observada')
+    print(cont1, cont2, cont3, cont4, cont5)
+    tot = ((1/frecEsp)*((cont1-frecEsp)**2+(cont2-frecEsp)**2+(cont3-frecEsp)**2+(cont4-frecEsp)**2+(cont5-frecEsp)**2))
+    print("Valor de X es")
+    print(tot)
     if tot < chi:
-        print('Los numeros no pertenecen a una distribucion uniforme')
+        print('Los numeros pertenecen a una distribucion uniforme')
     else:
         print('Los numeros no pertenecen a una distribucion uniforme')
