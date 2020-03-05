4738bd54eb7bed14baa81b4f90d15ff004112f7b
simsmilton <miltonrdz25@hotmail.com>
2020-03-05 10:43:45 -0600

    enter

---
+++ modelar_sistema/milton.py
@@ -0,0 +1,15 @@
+from poisson.py import poisson
+def milton():
+    if Fila1<=5:
+        Ts1 = Ts1-delta
+        if (Ts1==0):
+            Fila1 =Fila1 +1
+            Na1 = Na1 +1
+            #paso 1
+            poisson()
+            Ts1 = VP
+    else:
+        To1 = To1+delta
+    while LCajas == 0 and Fila1>5 and Fila2>5:
+        Ret = True
+    #paso 4
