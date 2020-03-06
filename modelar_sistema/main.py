1571834a9c17179e0e075ec62a299e1f33927339
netotz <neto.otz@hotmail.com>
2020-03-06 12:00:34 -0600

    Inicializar variables

--- modelar_sistema/main.py
+++ modelar_sistema/main.py
@@ -2,4 +2,37 @@
 Módulo para ejecutar tarea.
 '''
 
+from questionary import text
+
+from validaciones import is_valid_time
+from poisson import poisson
+
+tl = text(
+    'Ingresar límite de tiempo: ',
+    validate=lambda s: True if is_valid_time(s) else 'Por favor ingrese un valor válido.',
+    qmark='>'
+).ask()
+
+TLimite = float(tl)
+LCajas = 30
+TAsigC = 60
+Ret = False
+Reloj = 0
+TC = 0
+
+Ts1 = poisson()
+Ts2 = poisson()
+
+delta = min(Ts1, Ts2)
+
+Reloj += delta
+TC += delta 
+
+if TC >= TAsigC:
+	TC = TC - TAsigC
+	LCajas = LCajas + 30
+	Ret = False
+	if Fila2 != 0:
+		LCajas = LCajas - Fila2
+		Fila2 = 0
 
